e4148852c1c502f2b0df00b7e6ea67fb51d66b19
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-09-18 00:25:33 +0100

    TLS cert and key included

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gRPC-weather-data-fetcher
-An Endpoint that efficiently serves weather data via gRPC and HTTP by combining Protocol Buffers and the Gorilla Mux router.
+An Endpoint that efficiently serves secured weather data via gRPC and HTTP by combining Protocol Buffers, the Gorilla Mux router, and TLS (Transport Layer Security) encryption.
+
+Using a self-signed TLS certicate that is PEM encoded but suitable for development purposes only.
 
 ## Automating Program Compilation with a Makefile
 - To generate code from weather.proto file simply use: make compile
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -12,10 +14,11 @@ import (
 
 	pb "github.com/Osagie-Godstand/gRPC-weather-data-fetcher/api/v1"
 	"github.com/Osagie-Godstand/gRPC-weather-data-fetcher/internal/server"
+
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
@@ -26,7 +29,33 @@ func main() {
 	grpcServerAddress := os.Getenv("GRPC_SERVER_ADDRESS")
 	httpServerPort := os.Getenv("HTTP_SERVER_PORT")
 
-	grpcServer := grpc.NewServer()
+	// Loading TLS certificates
+	certFile := "tls/cert.pem"
+	keyFile := "tls/key.pem"
+
+	// Loading the server's certificate
+	cert, err := os.ReadFile(certFile)
+	if err != nil {
+		log.Fatalf("Failed to load server certificate: %v", err)
+	}
+
+	// Creating a certificate pool and add the server's certificate
+	certPool := x509.NewCertPool()
+	if ok := certPool.AppendCertsFromPEM(cert); !ok {
+		log.Fatalf("Failed to append server certificate to the certificate pool")
+	}
+
+	// Creating a TLS configuration with the certificate pool
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	// Creating a new gRPC server with TLS credentials
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		log.Fatalf("Failed to load TLS certificates: %v", err)
+	}
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 
 	pb.RegisterWeatherServiceServer(grpcServer, &server.WeatherServer{})
 
@@ -53,7 +82,7 @@ func main() {
 
 		conn, err := grpc.Dial(
 			grpcServerAddress,
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
+			grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)),
 		)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
